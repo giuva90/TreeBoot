064b0edfa0e95a0cd12b2902e1f541b707868bfc
Giovanni Rizzo <rizzogiovanni90@gmail.com>
2017-07-03 23:23:00 +0200

    Added decision tree class, ready to be edited

---
+++ DTC/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+
+if sys.version_info[0] == 3:
+	from DTC.DecisionTree import __version__
+	from DTC.DecisionTree import __author__
+	from DTC.DecisionTree import __date__
+	from DTC.DecisionTree import __url__
+	from DTC.DecisionTree import __copyright__
+
+	from DTC.DecisionTree import DecisionTree
+	from DTC.DecisionTree import EvalTrainingData
+	from DTC.DecisionTree import DTIntrospection
+	from DTC.DecisionTree import TrainingDataGeneratorNumeric
+	from DTC.DecisionTree import TrainingDataGeneratorSymbolic
+
+	from  DTC.DecisionTree import DTNode
+
+else:
+	from DTC import __version__
+	from DTC import __author__
+	from DTC import __date__
+	from DTC import __url__
+	from DTC import __copyright__
+
+	from DTC import DecisionTree
+	from DTC import EvalTrainingData
+	from DTC import DTIntrospection
+	from DTC import TrainingDataGeneratorNumeric
+	from DTC import TrainingDataGeneratorSymbolic
+
+	from DTC import DTNode
